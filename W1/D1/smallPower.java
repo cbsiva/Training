c7f96e253e076c1b96e6f27552e7cb7ba3fcd35b
cb-sivanantham <cb-sivanantham@cb-sivananthams-MacBook-Pro.local>
2015-12-19 15:03:57 +0530

    Week1 Day1

--- W1/D1/smallPower.java
+++ W1/D1/smallPower.java
@@ -4,11 +4,13 @@ public class smallPower{
 	public static void main(String argv[]){
 		 
 		 Scanner sc = new Scanner(System.in);
+		 //Read X
 		 System.out.print("X=");
 		 int ix = sc.nextInt();
+		 //Read Y
 		 System.out.print("Y=");
 		 int iy = sc.nextInt();
-
+		 //Result
 		System.out.println("Smallest Power: "+Math.floor(Math.log(iy)/Math.log(ix)));
 
 	}
