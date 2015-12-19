4a7beef13ce1db6d068501603bd6e54990a5a9e8
cb-sivanantham <cb-sivanantham@cb-sivananthams-MacBook-Pro.local>
2015-12-19 16:59:28 +0530

    week1 day2

---
+++ W1/D2/pattern.java
@@ -0,0 +1,8 @@
+public class pattern{
+	public static void main(String argv[]){
+		System.out.println(" \t \t \t1");
+		System.out.println(" \t \t1\t2\t1");
+		System.out.println(" \t1\t2\t3\t2\t1");
+		System.out.println("1\t2\t3\t4\t3\t2\t1");
+	}
+}
