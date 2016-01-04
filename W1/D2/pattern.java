d06a4463b606093da0f383168b1bb94256d188fb
cb-sivanantham <cb-sivanantham@cb-sivananthams-MacBook-Pro.local>
2016-01-04 11:59:53 +0530

    Updated

--- W1/D2/pattern.java
+++ W1/D2/pattern.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+import java.math.*;
 public class pattern{
+
+	public static String space(int k){
+		return new String(new char[k]).replace("\0", " ");
+	}
+
 	public static void main(String argv[]){
-		System.out.println(" \t \t \t1");
-		System.out.println(" \t \t1\t2\t1");
-		System.out.println(" \t1\t2\t3\t2\t1");
-		System.out.println("1\t2\t3\t4\t3\t2\t1");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Number of Line: ");
+		int k=sc.nextInt();
+
+		BigInteger sum = new BigInteger("1");
+		for(int i=1;i<=k;i++){
+			System.out.println(space(k-i)+sum.pow(2));
+			sum = sum.multiply(new BigInteger("10")).add(new BigInteger("1"));
+		}
+
 	}
 }
---
+++ W1/D4/Animal/Animal.java
@@ -0,0 +1,32 @@
+class animal{
+
+}
+
+class mammal{
+
+}
+
+class bird{
+
+}
+
+class bat extends mammal{
+
+}
+
+class dog extends animal{
+
+}
+
+class cow extends mammal{
+
+}
+
+class ostrich extends bird{
+
+}
+
+class parrot extends bird{
+
+}
+
